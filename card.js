dc9e1fab45bf7a6f4add386ec438411d39f007d0
cjvirtucio87 <cjv287@gmail.com>
2016-09-09 09:52:20 -0700

    Passed cards to view.

--- card.js
+++ card.js
@@ -1,30 +1,34 @@
+// Holds state regarding the cards.
 var model = {
   init:  function(){
     this.createCards();
   },
+  // Modifying state.
+  createCards: function() {
+    for(var i = 0; i < 2; i++) {
+      this.cards.push(this.colors[i]);
+      this.cards.push(this.colors[i]);
+    }
+  },
+
+  // Properties.
   totalMatches: 0,
   currentMatch: 0,
   colors: ["red", "yellow"],
   cards: [],
   faceDown: "black",
-  createCards: function() {
-    for(var i = 0; i < 2; i++) {
-      cards.push(this.colors[i])
-      cards.push(this.colors[i])
-    }
-  }
+};
 
-}
-
+// Presents cards (modifying DOM).
 var view = {
-  init: function() {
-    model.cards.forEach(function(card){
-      $('.container').append("<div class='card' id")
+  init: function(cardsData) {
+    cardsData.forEach(function(card){
+      $('.container').append("<div class='card'" + "id=" + "card-" + card + " </div>");
     });
   }
-}
+};
 
-
+// Gets data from model, passes data to view.
 var controller = {
 
   init: function() {
@@ -32,7 +36,4 @@ var controller = {
     view.init(model.cards);
   }
 
-
-}
-
-
+};
